a5dc82b78e884c0551de18ac5f813dc33c1c4210
James <jamesnjunge45@gmail.com>
2024-01-20 14:16:51 +0300

    Protecting routes

--- API/routes/api.php
+++ API/routes/api.php
@@ -22,7 +22,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('jwt')->group(function () {
 Route::apiResource('companies', CompanyController::class);
 Route::apiResource('contacts', ContactController::class);
 Route::apiResource('deals', DealController::class);
 Route::apiResource('leads', LeadController::class);
+});
